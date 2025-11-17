d87cc144d3af54c6fdafef4368f172f025b16cb4
vasilievyakov <vasilievgpt@gmail.com>
2025-11-17 20:58:49 +0100

    refactor: remove legacy pages router, switch to app router only

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        stream: false,
-        url: false,
-        zlib: false,
-        http: false,
-        https: false,
-        assert: false,
-        os: false,
-        path: false,
-      };
-    }
-    return config;
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
